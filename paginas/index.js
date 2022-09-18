63eadb4abdc63cb438c87c895d26a54a1500963c
Agustin Auri <agustin_auri@hotmail.com>
2022-09-18 18:56:11 -0300

    inico de maquetacion del carrtio

--- paginas/index.js
+++ paginas/index.js
@@ -1,9 +1,35 @@
 
-const resta = (turnoDelPaciente, totalDeTurnos) => turnoDelPaciente - totalDeTurnos ;
-const espera = x => x * 15;
+const productos = [
+    { name: "remera", price: 2500 },
+    { name: "pantalon", price: 7500 },
+    { name: "vela", price: 1500 },
+    { name: "disco", price: 1000 },
+    { name: "libro", price: 1500 },
+];
 
-let turnoPaciente = parseInt(prompt("ingrese su turno"));
-let turnos = parseInt(prompt("ingrerse total de turnos"));
+console.log(
+    items.sort((a, b) => {
+        if (a.name < b.name) {
+            return -1;
+        }
+        if (a.name > b.name) {
+            return 1;
+        }
 
-let tiempoDeEspera = espera(resta(turnos, turnoPaciente));
-alert(tiempoDeEspera + " minutos de espera")
+        return 0;
+    })
+);
+
+let encontrado = productos.find(productos => productos.nombre === "Javascript");
+console.log(encontrado); 
+
+let precio = parseInt(prompt("ingrese el producto a buscar"));
+let filtrados = productos.filter((productos) => cursos.precio > precio);
+filtrados.forEach((productos) => {
+    let mensaje = `
+    nombre: ${curso.nombre}
+    $${curso.precio}
+    `;
+
+    alert(mensaje);
+});
